63d3e9a15bcdb324a1f75245acccd0cb2123482c
xinlin4 <xinlinchenzhang@gmail.com>
2026-04-19 16:25:33 +0200

    fix cors patterns

--- src/main/java/com/example/demo/Proyecto/Security/SecurityConfig.java
+++ src/main/java/com/example/demo/Proyecto/Security/SecurityConfig.java
@@ -62,7 +62,8 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        //configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
