5bcd6cb5ea9738c1a07d635d1ee50ab58eb20b2a
xinlin4 <xinlinchenzhang@gmail.com>
2026-05-15 23:05:04 +0200

    Nuevo estado añadido a pedido

--- src/main/java/com/example/demo/Proyecto/Enum/Estado.java
+++ src/main/java/com/example/demo/Proyecto/Enum/Estado.java
@@ -2,13 +2,15 @@ package com.example.demo.Proyecto.Enum;
 
 public enum Estado {
     PENDIENTE,
+    ACEPTADO,
     REPARTO,
     COMPLETADO,
     CANCELADO;
 
     public boolean puedeTransicionarA(Estado nuevo) {
-        return switch(this) {
-            case PENDIENTE -> nuevo == REPARTO || nuevo == CANCELADO;
+        return switch (this) {
+            case PENDIENTE -> nuevo == ACEPTADO || nuevo == CANCELADO;
+            case ACEPTADO -> nuevo == REPARTO || nuevo == CANCELADO;
             case REPARTO -> nuevo == COMPLETADO;
             case COMPLETADO -> false;
             case CANCELADO -> false;
