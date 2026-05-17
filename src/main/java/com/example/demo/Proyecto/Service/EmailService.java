1036db73afc5b997fb079366ab7b526bc92c73df
xinlin4 <xinlinchenzhang@gmail.com>
2026-05-17 19:47:42 +0200

    Recuperacion con brevo

--- src/main/java/com/example/demo/Proyecto/Service/EmailService.java
+++ src/main/java/com/example/demo/Proyecto/Service/EmailService.java
@@ -1,24 +1,53 @@
 package com.example.demo.Proyecto.Service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class EmailService {
 
-    @Autowired
-    private JavaMailSender mailSender;
+    @Value("${brevo.api.key}")
+    private String apiKey;
 
-    public void enviarCorreo(String destino, String asunto, String contenido) {
+    public void enviarCorreo(
+            String destino,
+            String asunto,
+            String contenido) {
 
-        SimpleMailMessage mensaje = new SimpleMailMessage();
+        RestTemplate restTemplate = new RestTemplate();
 
-        mensaje.setTo(destino);
-        mensaje.setSubject(asunto);
-        mensaje.setText(contenido);
+        String url = "https://api.brevo.com/v3/smtp/email";
 
-        mailSender.send(mensaje);
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        headers.set("api-key", apiKey);
+
+        Map<String, Object> body = Map.of(
+
+                "sender", Map.of(
+                        "name", "Recuperar Coviran",
+                        "email", "xinlinchenzhang@gmail.com"),
+
+                "to", List.of(
+                        Map.of("email", destino)),
+
+                "subject", asunto,
+
+                "textContent", contenido);
+
+        HttpEntity<Map<String, Object>> request = new HttpEntity<>(body, headers);
+
+        restTemplate.exchange(
+                url,
+                HttpMethod.POST,
+                request,
+                String.class);
     }
 }
